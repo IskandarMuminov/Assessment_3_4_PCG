657d1b3401a53437603a5e84a3f9f053bd0ccd1b
Zeun456 <Ellie.J.Song@student.uts.edu.au>
2024-10-11 17:10:00 +1100

    testworldsubsystem

---
+++ Source/PCGA/TestWorldSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "TestWorldSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PCGA_API UTestWorldSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+	
+};
